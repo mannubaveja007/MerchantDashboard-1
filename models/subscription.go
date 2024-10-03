16f5b3f77dc1e3aa3e7c48f8e2ea7a1aa070ef50
mannubaveja007 <142614912+mannubaveja007@users.noreply.github.com>
2024-10-03 23:23:39 +0530

    Update subscription.go

--- models/subscription.go
+++ models/subscription.go
@@ -1,7 +1,9 @@
 package models
 
 type Subscription struct {
-	PlanID     string  `json:"plan_id"`
-	CustomerID string  `json:"customer_id"`
-	Price      float64 `json:"price"`
+	PlanID      string  `json:"plan_id"`
+	CustomerID  string  `json:"customer_id"`
+	Price       float64 `json:"price"`
+	Category    string  `json:"category"`
+	PlanDetails string  `json:"plan_details"`
 }
